2cc8676ce3465f3f7df003e2d7433967a44d3d02
alice-0-kim <aliceykim0828@gmail.com>
2018-05-18 10:15:49 -0700

    apply debouncing scroll events

--- js/navigation.sticky.js
+++ js/navigation.sticky.js
@@ -2,27 +2,49 @@
 
   Drupal.behaviors.megatronStickyNav = {
     attach: function(context, settings) {
-      window.onscroll = function() { stickyNav() };
+
       var navbar = document.getElementById("ubc7-unit-menu");
       var unitbar = document.getElementById("ubc7-unit");
       var stickynav = navbar.offsetTop;
       var stickyunit = unitbar.offsetTop;
-      function stickyNav() {
-        if (window.pageYOffset >= stickynav) {
+      var latestKnownScrollY = 0;
+      var ticking = false;
+
+      function onScroll() {
+        latestKnownScrollY = window.scrollY;
+        requestTick();
+      }
+
+      function requestTick() {
+        if(!ticking) {
+          window.requestAnimationFrame(update);
+        }
+        ticking = true;
+      }
+
+      function update() {
+        ticking = false;
+        var currentScrollY = latestKnownScrollY;
+
+        //toggle classes
+        if (currentScrollY >= stickynav) {
           navbar.classList.add("navigation-is-sticky");
           document.body.classList.add("unit-menu-is-sticky");
         } else {
           navbar.classList.remove("navigation-is-sticky");
           document.body.classList.remove("unit-menu-is-sticky");
         };
-        if (window.pageYOffset >= stickyunit) {
+        if (currentScrollY >= stickyunit) {
           unitbar.classList.add("navigation-is-sticky");
           document.body.classList.add("unit-area-is-sticky");
         } else {
           unitbar.classList.remove("navigation-is-sticky");
           document.body.classList.remove("unit-area-is-sticky");
         }
+
       }
+      // window.requestAnimationFrame(update);
+      window.addEventListener('scroll', onScroll, false);
     }
   }
 
