9b7c2d576da68b6f5be97be68e4674be1b86f832
Darko Hrgovic <darko.hrgovic@ubc.ca>
2019-01-21 14:18:56 -0800

    Removed toggle behaviour in flyout js that was killed by webform conditionals

--- js/off.canvas.flyout.js
+++ js/off.canvas.flyout.js
@@ -4,25 +4,41 @@
     attach: function (context, settings) {
       // Primary flyout toggle.
       if (document.getElementsByClassName("flyout-toggle").length > 0) {
-        var pushTrigger = document.getElementsByClassName("flyout-toggle");
+        var pushTrigger = document.getElementById("flyout-button");
+        var closeTrigger = document.getElementsByClassName("flyout__close");
+        var pushMask = document.getElementById("off-canvas-mask-flyout");
         var pushPanel = document.getElementById("off-canvas-flyout");
         var pushContent = document.getElementById("pushed-content-flyout");
-        var pushMask = document.getElementById("off-canvas-mask-flyout");
         var activeClass = "off-canvas-flyout--is-active";
         var animatingClass = "off-canvas-flyout--is-animating";
         var i;
         document.body.classList.add("has-off-canvas-flyout");
-        for (i = 0; i < pushTrigger.length; i++) {
-          pushTrigger[i].addEventListener("click", function () {
-            document.body.classList.toggle(activeClass);
-            pushPanel.classList.toggle(activeClass);
-            pushContent.classList.toggle(activeClass);
-            pushMask.classList.toggle(activeClass);
-            document.body.classList.add(animatingClass);
-            pushPanel.classList.add(animatingClass);
-            pushContent.classList.add(animatingClass);
-            pushMask.classList.add(animatingClass);
-          });
+        pushTrigger.addEventListener("click", flyOutOpen, true);
+        pushMask.addEventListener("click", flyOutClose, true);
+        for (i = 0; i < closeTrigger.length; i++) {
+          closeTrigger[i].addEventListener("click", flyOutClose, true);
+        }
+        function flyOutOpen(e) {
+          console.log('open clicked');
+          document.body.classList.add(activeClass);
+          pushPanel.classList.add(activeClass);
+          pushContent.classList.add(activeClass);
+          pushMask.classList.add(activeClass);
+          document.body.classList.remove(animatingClass);
+          pushPanel.classList.remove(animatingClass);
+          pushContent.classList.remove(animatingClass);
+          pushMask.classList.remove(animatingClass);
+        }
+        function flyOutClose(e) {
+          console.log('close clicked');
+          document.body.classList.remove(activeClass);
+          pushPanel.classList.remove(activeClass);
+          pushContent.classList.remove(activeClass);
+          pushMask.classList.remove(activeClass);
+          document.body.classList.add(animatingClass);
+          pushPanel.classList.add(animatingClass);
+          pushContent.classList.add(animatingClass);
+          pushMask.classList.add(animatingClass);
         }
         // Old Safari requires 'webkit' prefix.
         pushPanel.addEventListener("webkitTransitionEnd", finishedAnimating);
